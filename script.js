0581b7ad9c1f523416edec9c1d41b4f0b32c5158
kingklaus21 <nicholasunderwood2005@gmail.com>
2024-09-26 17:47:31 -0400

    Create file insert and viewer for images

---
+++ script.js
@@ -0,0 +1,53 @@
+// look up mdm on forms to add multiple images
+
+document.getElementById('fileInput').addEventListener('change', function(event) {
+    const file = event.target.files[0];
+    const previewImage = document.getElementById('previewImage');
+
+    if (file) {
+        const reader = new FileReader();
+        
+        reader.onload = function(e) {
+            previewImage.src = e.target.result;
+            previewImage.style.display = 'block';
+        }
+
+        reader.readAsDataURL(file);
+    } else {
+        previewImage.style.display = 'none';
+    }
+});
+
+if (typeof(Storage) !=="undefined") {
+    console.log('storage will work');
+}
+else {
+    console.log('storage will not work');
+}
+
+
+
+
+const userNameText = document.querySelector(".userName");
+const saveNameButton = document.querySelector(".saveNameBtn");
+
+saveNameButton.addEventListener("click", () => {
+    const userName = document.querySelector(".name").ariaValueMax;
+    userNameText.textContent = userName;
+    localStorage.setItem("name", userName);
+});
+
+function displayUserName () {
+    const nameFromLocalStorage = localStorage.getItem("name");
+
+    if (nameFromLocalStorage) {
+        userNameText.textContent = nameFromLocalStorage;
+    }
+    else {
+        userNameText.textContent = "No name data in local storage";
+    }
+}
+
+displayUserName();
+
+
