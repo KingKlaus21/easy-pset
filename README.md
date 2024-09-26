9ec894ba390acc57e38c8700cdf0ccffa609fd1d
kingklaus21 <nicholasunderwood2005@gmail.com>
2024-09-26 18:17:21 -0400

    Rename repo and start using local storage

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # easy_pset
 Got angry seeing expensive pdf editors and stuff that didn't reformat files so I made this
+
+https://kingklaus21.github.io/easy-pset/
--- script.js
+++ script.js
@@ -28,26 +28,28 @@ else {
 
 
 
-const userNameText = document.querySelector(".userName");
-const saveNameButton = document.querySelector(".saveNameBtn");
+document.addEventListener("DOMContentLoaded", () => {
+    const userNameText = document.querySelector(".userName");
+    const saveNameButton = document.querySelector(".saveNameBtn");
 
-saveNameButton.addEventListener("click", () => {
-    const userName = document.querySelector(".name").ariaValueMax;
-    userNameText.textContent = userName;
-    localStorage.setItem("name", userName);
+    saveNameButton.addEventListener("click", () => {
+        const userName = document.querySelector(".name").value;
+        userNameText.textContent = userName;
+        localStorage.setItem("name", userName);
+    });
+
+    function displayUserName() {
+        const nameFromLocalStorage = localStorage.getItem("name");
+
+        if (nameFromLocalStorage) {
+            userNameText.textContent = nameFromLocalStorage;
+        } else {
+            userNameText.textContent = "No name data in local storage";
+        }
+    }
+
+    displayUserName();
 });
 
-function displayUserName () {
-    const nameFromLocalStorage = localStorage.getItem("name");
-
-    if (nameFromLocalStorage) {
-        userNameText.textContent = nameFromLocalStorage;
-    }
-    else {
-        userNameText.textContent = "No name data in local storage";
-    }
-}
-
-displayUserName();
 
 
