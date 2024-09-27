068f9f7d0142abd22fcd67ef7b859279f74c2883
kingklaus21 <nicholasunderwood2005@gmail.com>
2024-09-26 22:22:52 -0400

    Store text using local storage that is automatically displayed after reloading

--- script.js
+++ script.js
@@ -28,37 +28,79 @@ else {
 
 
 
-document.addEventListener("DOMContentLoaded", () => {
-    const userNameText = document.querySelector(".userName");
-    const saveNameButton = document.querySelector(".saveNameBtn");
-    const nameInput = document.querySelector(".name");
+// document.addEventListener("DOMContentLoaded", () => {
+//     const userNameText = document.querySelector(".userName");
+//     const saveNameButton = document.querySelector(".saveNameBtn");
+//     const nameInput = document.querySelector(".name");
 
-    // Check if elements are correctly selected
-    console.log("userNameText:", userNameText);
-    console.log("saveNameButton:", saveNameButton);
-    console.log("nameInput:", nameInput);
+//     // Check if elements are correctly selected
+//     console.log("userNameText:", userNameText);
+//     console.log("saveNameButton:", saveNameButton);
+//     console.log("nameInput:", nameInput);
 
-    saveNameButton.addEventListener("click", () => {
-        const userName = nameInput.value;
-        console.log("Entered name:", userName);  // Check the entered name
-        userNameText.textContent = userName;
-        localStorage.setItem("name", userName);
-    });
+//     saveNameButton.addEventListener("click", () => {
+//         const userName = nameInput.value;
+//         console.log("Entered name:", userName);  // Check the entered name
+//         userNameText.textContent = userName;
+//         localStorage.setItem("name", userName);
+//     });
 
-    function displayUserName() {
-        const nameFromLocalStorage = localStorage.getItem("name");
-        console.log("Name from localStorage:", nameFromLocalStorage);  // Check local storage value
+//     function displayUserName() {
+//         const nameFromLocalStorage = localStorage.getItem("name");
+//         console.log("Name from localStorage:", nameFromLocalStorage);  // Check local storage value
 
-        if (nameFromLocalStorage) {
-            userNameText.textContent = nameFromLocalStorage;
-        } else {
-            userNameText.textContent = "No name data in local storage";
-        }
-    }
+//         if (nameFromLocalStorage) {
+//             userNameText.textContent = nameFromLocalStorage;
+//         } else {
+//             userNameText.textContent = "No name data in local storage";
+//         }
+//     }
 
-    displayUserName();
+//     displayUserName();
+// });
+
+
+
+const believeText = document.querySelector(".believe");
+
+believeText.addEventListener("input", () => {
+    localStorage.setItem("believe", believeText.value);
 });
 
+function displayBelieveText() {
+    const believeStorageValue = localStorage.getItem("believe");
+
+    if (believeStorageValue) {
+        console.log('the value is ' + believeStorageValue);
+        // believeText.textContent = believeStorageValue;
+        believeText.value = believeStorageValue;
+    }
+    else {
+        console.log('believe not working yet');
+    }
+}
+
+displayBelieveText();
 
 
 
+
+
+
+// function updateValue(e) {
+//     localStorage.setItem("believe", e.target.value);
+//     console.log(e.target.value);
+// }
+
+// function displayUpdateValue() {
+//     const updatedBelieveValue = localStorage.getItem("believe");
+
+//     if (updatedBelieveValue) {
+//         console.log(believeText.value);
+//     }
+//     else {
+//         console.log('believe has no content yet');
+//     }
+// }
+
+// updateValue(e);
